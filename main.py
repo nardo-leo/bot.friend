2e36d2af281975fd75d240bdabf6036fd677c475
nardo-leo <jora.karuzo@gmail.com>
2020-11-30 10:26:17 +0300

    Get text from Bolshoi and compare it with current one

---
+++ main.py
@@ -0,0 +1,15 @@
+import requests
+import bs4
+
+
+url = 'https://www.bolshoi.ru/visit/buyingnew/'
+response = requests.get(url)
+soup = bs4.BeautifulSoup(response.text, 'html.parser')
+
+dec_text = 'График продажи билетов на НОЯБРЬ и ДЕКАБРЬ 2020 г.\n\t\t\t с учетом ограничения посетителей до 25% вместимости зрительного зала'
+text_block = soup.find('div', attrs={'class', 'text_content_block'})
+a = text_block.find_all('a')[25]
+cur_text = a.text
+
+if (cur_text != dec_text):
+    # send_message
